6543ea8758f93dd9ac6801030ff17904c23844df
ImSav22 <saverio.decandia2210@gmail.com>
2022-05-14 17:13:23 +0200

    Implementazione prima parte funzione Tentativo

--- src/main/java/it/uniba/app/Controller.java
+++ src/main/java/it/uniba/app/Controller.java
@@ -68,5 +68,55 @@ public class Controller {
             m.stampaMatrice(maxTentativi, numCaratteri);
         }
     }
+
+    /**Permette di effettuare un tentativo per indovinare la parola segreta */
+    public void Tentativo(Giocatore g, String s, Paroliere p)
+    {
+        if(!p.getParolaSegreta().equals(null))
+        {
+            if(g.getTentativi()<maxTentativi)
+            {
+                boolean flagCorrect=false;
+
+                if(s.length()<numCaratteri)  
+                {
+                    System.out.println("Parola troppo corta. La parola deve contenere " + numCaratteri + " lettere.");
+                }         
+                else if(s.length()>numCaratteri)
+                {
+                    System.out.println("Parola troppo lunga. La parola deve contenere " + numCaratteri + " lettere.");
+                }
+                else
+                {
+                    s.toUpperCase();
+
+                    for(int i=0; i<numCaratteri && !flagCorrect; i++)
+                    {
+                        if(s.charAt(i)<'A' || s.charAt(i)>'Z')
+                        {
+                            flagCorrect=true;
+                        }
+                    }
+                    if(flagCorrect)
+                    System.out.println("Parola non valida. La parola può contenere solo caratteri alfabetici.");
+                }
+            }
+            else
+            {
+                System.out.println("Numero massimo di tentativi raggiunto. Avvia una nuova partita.");
+            }
+        }
+        else 
+        {
+            System.out.println("Parola segreta mancante. Imposta una parola segreta per giocare.");
+        }
+        /**Implementati i controlli sulla correttezza della parola inserita e sullo stato del gioco
+         * Da sviluppare: 
+         * Verifica se la parola è stata indovinata;
+         * Colori delle lettere;
+         * Implementazione della stampa a video.
+        */
+         
+    }
 }
 
