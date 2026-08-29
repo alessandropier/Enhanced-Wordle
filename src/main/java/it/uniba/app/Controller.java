2785c3ba28d3b4038b39eb35a16c4c2673ce8dde
alessandropier <alessandropiergiovanni001@gmail.com>
2026-08-29 12:30:15 +0200

    beautify wordle function (#1)

--- src/main/java/it/uniba/app/Controller.java
+++ src/main/java/it/uniba/app/Controller.java
@@ -1,7 +1,5 @@
 package it.uniba.app;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
